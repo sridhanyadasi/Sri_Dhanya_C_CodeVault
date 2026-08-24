83fa6823d884d7694bc808cd2fe90cae2dd1fdb4
Sri Dhanya <dasidhanya@gmail.com>
2026-08-24 15:19:09 +0530

    Add files via upload

---
+++ Basics/First_Hello_Program/hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+printf("Hello, world! \n");   // \n  -> new line  -->   prints output in a next line
+printf("Hello,  C \n");
+
+printf("Hello, world! \t");   //  \t -> horizontal tab  -->  gives a tab space
+printf("Hello,  c \t");
+
+printf("Hello, world!");
+printf("Hello,  c");
+
+}
